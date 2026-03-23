1412c912dbbd7355614f42ad095e7d68747b4277
nicolly <saantos.nick1606@gmail.com>
2026-03-23 08:06:35 -0300

    arrumando sintaxe

--- src/routes/pdfRoute.js
+++ src/routes/pdfRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as controller from '../controllers/pdfController.js';;
+import * as controller from '../controllers/pdfController.js';
 
 const router = express.Router();
 
